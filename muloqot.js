0fae358bc6243a8e6906b696cb33338675273f0a
Ulugbek <uurinov044@gmail.com>
2023-03-05 16:46:55 +0500

    yangilash2

--- muloqot.js
+++ muloqot.js
@@ -1,25 +1,25 @@
 "use strict";
-// const place=consist("are you here");
-// const age = prompt("Nichcha yoshsan", "");
-// console.log("Men " + age + " yoshman");
-// const YoqtirganRang = [];
-// YoqtirganRang[0] = prompt("Yoqtirgan ranging qaysi #1", "");
-// YoqtirganRang[1] = prompt("Yoqtirgan ranging qaysi #2", "");
-// YoqtirganRang[2] = prompt("Yoqtirgan ranging qaysi #3", "");
-// console.log(YoqtirganRang);
-// const Malumot = [];
-// Malumot[0] = prompt("Yosh", "");
-// Malumot[1] = prompt("Ism", "");
-// Malumot[2] = prompt("Familiya", "");
-// console.log("Yoshim " + Malumot[0] + " da");
-// console.log("Ismim " + Malumot[1]);
-// console.log("Familiyam " + Malumot[2]);
-// const Youtube = prompt("Sen yoqtirgan youtuber kim ", "");
-// console.log(`https://youtube.com/${Youtube}`);
-// console.log(`string text line 1
-// string text line 2`);
-// "string text line 1
-// string text line 2"
+const place=consist("are you here");
+const age = prompt("Nichcha yoshsan", "");
+console.log("Men " + age + " yoshman");
+const YoqtirganRang = [];
+YoqtirganRang[0] = prompt("Yoqtirgan ranging qaysi #1", "");
+YoqtirganRang[1] = prompt("Yoqtirgan ranging qaysi #2", "");
+YoqtirganRang[2] = prompt("Yoqtirgan ranging qaysi #3", "");
+console.log(YoqtirganRang);
+const Malumot = [];
+Malumot[0] = prompt("Yosh", "");
+Malumot[1] = prompt("Ism", "");
+Malumot[2] = prompt("Familiya", "");
+console.log("Yoshim " + Malumot[0] + " da");
+console.log("Ismim " + Malumot[1]);
+console.log("Familiyam " + Malumot[2]);
+const Youtube = prompt("Sen yoqtirgan youtuber kim ", "");
+console.log(`https://youtube.com/${Youtube}`);
+console.log(`string text line 1
+string text line 2`);
+"string text line 1
+string text line 2"
 const numberOfSeries = +prompt("Nechta serial ko’rdingiz?", "");
 if (numberOfSeries < "5") {
   console.log("Kam serial ko'ribsiz");
---
+++ topshiriq1.js
@@ -0,0 +1,15 @@
+const numberOfSeries = +prompt("Nechta serial ko'rdingiz");
+const seriesDB = {
+  count: numberOfSeries,
+  series: {},
+  actors: {},
+  genres: [],
+  private: false,
+};
+const a = prompt("Oxirgi ko'rgan serialingiz"),
+  b = +prompt("Nechi baxo berasiz"),
+  c = prompt("Oxirgi ko'rgan serialingiz"),
+  d = +prompt("Nechi baxo berasiz");
+seriesDB.series.a = b;
+seriesDB.series.c = d;
+console.log(seriesDB);
